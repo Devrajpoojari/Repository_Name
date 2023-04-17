636f56f1483d9872de8d50169443b01d6f6968b4
Devrajpoojari <devrajpoojari143@gmail.com>
2023-04-17 14:09:23 +0530

    Added new Class called demo for doing square

---
+++ src/com/Demo.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Demo {
+
+	public static int square(int n) {
+		return n * n;
+	}
+
+	public static void main(String[] args) {
+
+		int a = square(20);
+		System.out.println(a);
+
+	}
+
+}
